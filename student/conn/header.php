25d922920632d901375323cc001cde0aad1c4416
FartInhaler <129055537+FartInhaler@users.noreply.github.com>
2024-01-30 12:19:04 +0100

    d

---
+++ student/conn/header.php
@@ -0,0 +1,14 @@
+<?php
+$table_header = '<table id="students">
+                    <tr>
+                        <th>studentnr</th>
+                        <th>voornaam</th>
+                        <th>tussenvoegsel</th>
+                        <th>achternaam</th>
+                        <th>straat</th>
+                        <th>postcode</th>
+                        <th>woonplaats</th>
+                        <th>email</th>
+                        <th>klas</th>
+                        <th>geboortedatum</th>
+                    </tr>';
---
+++ student/db/database.php
@@ -0,0 +1,32 @@
+<?php
+
+//echo 'PDO-drivers ingesteld in PHP.INI: <br>';
+
+//print_r(PDO::getAvailableDrivers());
+
+//Initialisatie
+
+define('HOST', 'localhost');
+
+define('DATABASE', 'student');
+
+define('USER', 'webuser');
+
+define('PASSWORD','7q06DXjDr1Z3reXK');
+
+ 
+
+$dbconn='';
+
+//connectie maken
+
+try {
+    $dbconn = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8mb4", USER,PASSWORD);
+    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $e) {
+    echo $e->getMessage();
+    echo "verbinding NIET gemaakt<br>";
+}
+
