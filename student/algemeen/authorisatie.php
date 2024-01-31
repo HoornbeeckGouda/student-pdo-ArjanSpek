67d64d6b9e74e1d2162257594aff6e94ac7ffb6a
FartInhaler <129055537+FartInhaler@users.noreply.github.com>
2024-01-31 13:52:45 +0100

    yes

---
+++ student/algemeen/authorisatie.php
@@ -0,0 +1,13 @@
+<?php
+include 'inc/header.php';
+//database checken of inlognaam en wachtwoord overeenkomen
+if ($_POST=['submit']) {
+    $inlognaam=isset($_POST['inlognaam']) ? $_POST['inlognaam'] : '';
+    $wachtwoord=isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : '';
+}else {
+    header('refresh: 1, index.php');
+}
+$query = "SELECT  FROM gebruiker
+            where inlognaam='" . $inlognaam . "' and wachtwoord='" . $wachtwoord . "';";
+
+?>
--- student/algemeen/studenten.php
+++ student/algemeen/studenten.php
@@ -1,5 +1,5 @@
 <?php
-include './header.php';
+include 'inc/header.php';
 
 
 // initialiseren/declareren
@@ -55,5 +55,5 @@ $table_student = $table_header . $contentTable . "</table>";
 echo $table_student;
 
 
-include './Footer.php';
+include 'inc/Footer.php';
 ?>
